2eb13c4d75de3c2fab104c229b5e50468091e523
Mateus Wensch <wensch22@gmail.com>
2020-09-24 18:18:02 -0300

    Cypress test vue

---
+++ tests/e2e/specs/home_page.js
@@ -0,0 +1,8 @@
+// https://docs.cypress.io/api/introduction/api.html
+
+describe('Checking server', () => {
+  it('Visits the app root url', () => {
+    cy.visit('http://localhost:8080/')
+    // here see get api bracking bad
+  })
+})
